810388b190a09cb1d88e0f73623e0d59b9447e83
MFernando2522 <166775031+MFernando2522@users.noreply.github.com>
2025-04-15 16:57:41 -0400

    Update SuperStore.java

--- SuperStore.java
+++ SuperStore.java
@@ -7,10 +7,9 @@ class Product {
     // - A protected double field called price
     protected double price;
     // - A private final int productId
-    private int productId;
-
+    private final int productId;
     // - A public static int nextId to help generate unique IDs
-    public static int nextId;
+    public static int nextId = 0;
 
     // - A constructor that sets name, price, and assigns a unique ID
     /**
@@ -18,38 +17,37 @@ class Product {
      * @param name for the name of the product
      * @param price for the price of said thing to buy
      */
-    public constructorForProduct(String name, double price, int nextId) {
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
-        //THIS MIGHT BE WRONG... BUT it might work too cause it says ASSIGNS in the comment
-        this.productId = nextId;
+        this.productId = nextId++;
     }
 
     // - Public getters for all fields
-    public string getName() {
+    public String getName() {
         return name;
     }
 
-    public string getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public string getProductId() {
-        return nextId;
+    public int getProductId() {
+        return productId;
     }
 
     // - Override toString() to return formatted product details
     @Override
     public String toString() {
-        return "Product name: " + name " Product price: " + price;
+        return "Product name: " + name + " Product price: " + price + " Product ID: " + productId;
     }
 
     // - Override equals(Object o) to compare name and productId
     @Override
     public boolean equals(Object o) {
-        if (o instanceof productId) {
-            productId other = (productId) o;
-            return this.name.equals(other.name);
+        if (o instanceof Product) {
+            Product other = (Product) o;
+            return this.name.equals(other.name) && this.productId == other.productId;
         }
         return false;
     }
@@ -67,27 +65,25 @@ class Electronics extends Product {
      * Making a constructor yeauh
      * @param name for name of product, super
      * @param price for price of product
-     * @param nextId whats its specific id to find it idk
      * @param brand for brand type, has to be string
      * @param hasBattery a boolean t or f
      */
-    public constructorForElectronics(String name, double price, int nextId, String brand, boolean hasBattery) {
-       super(name, price, nextId);
-       this.brand = brand;
-       this.hasBattery = hasBattery;
-
+    public Electronics(String name, double price, String brand, boolean hasBattery) {
+        super(name, price);
+        this.brand = brand;
+        this.hasBattery = hasBattery;
     }
 
     // - Override toString() to include brand and battery status
     @Override
     public String toString() {
-        return "Brand: " + brand " Has battery: " + hasBattery;
+        return super.toString() + " Brand: " + brand + " Has battery: " + hasBattery;
     }
-    final warrantyInfo() {
-        System.out.println("Money :(");
+
+    public final void warrantyInfo() {
+        System.out.println("1-year warranty included.");
     }
 }
-
 
 // TODO: Define a subclass Grocery that extends Product
 class Grocery extends Product {
@@ -96,31 +92,26 @@ class Grocery extends Product {
     // - Add a private boolean field called isPerishable
     private boolean isPerishable;
 
-
     // - Constructor takes all fields and uses super for shared ones
     /**
      * ANOTHER CONSTRUCTOR
      * @param name - super method for name
      * @param price - super method for cost
-     * @param nextId - super method for nextId given
      * @param weight - double for how big in kg, so in double
      * @param isPerishable - t or f
      */
-    public constructorForGrocery (String name, double price, int nextId, double weight, boolean isPerishable) {
-        super(name, price, nextId);
+    public Grocery(String name, double price, double weight, boolean isPerishable) {
+        super(name, price);
         this.weight = weight;
         this.isPerishable = isPerishable;
-
     }
+
     // - Override toString() to include weight and perishability
     @Override
     public String toString() {
-        return "Weight: " + weight + " Is perished: " + isPerishable;
+        return super.toString() + " Weight: " + weight + " Is perishable: " + isPerishable;
     }
-
 }
-
-
 
 // TODO: Define a final class Toy that extends Product
 final class Toy extends Product {
@@ -132,50 +123,50 @@ final class Toy extends Product {
      * ANOTHER CONSTRUCTOR
      * @param name - super method for name
      * @param price - super method for cost
-     * @param nextId - super method for nextId given
      * @param minAge - int for minimum age kid can play w toy
      */
-    public constructorForGrocery(String name, double price, int nextId, int minAge) {
-        super(name, price, nextId);
-        this.minAge;
+    public Toy(String name, double price, int minAge) {
+        super(name, price);
+        this.minAge = minAge;
+    }
 
     // - Override toString() to include minAge
-        @Override
-        public String toString() {
-            return "minAge: " + minAge;
-        }
-
+    @Override
+    public String toString() {
+        return super.toString() + " Minimum Age: " + minAge;
+    }
 }
-
 
 // TODO: Define class SuperStoreTest with a main method
 class SuperStoreTest {
     public static void main(String[] args) {
         // - Create at least one instance of each subclass
-        //product bookInstanceOne = new Book();
-
-        Product product = new Product("Spoon",1.09,0879);
-        Electronics gamingThing = Electronics("3DS", 199, 6910, "Nintendo", true);
-        Grocery foods = new Grocery("Energy Drink",2.59,0459,1, true);
-        Toy plastic = new Toy("Lego",849.99,75192,4); //hehe actual price of lego set btw
-
+        Product product = new Product("Spoon", 1.09);
+        Electronics gamingThing = new Electronics("3DS", 199, "Nintendo", true);
+        Grocery foods = new Grocery("Energy Drink", 2.59, 1, true);
+        Toy plastic = new Toy("Lego", 849.99, 4);
 
         // - Store them in a Product[] array
-        Product[] products = {product, gamingThing, foods, plastic}
+        Product[] products = {product, gamingThing, foods, plastic};
+
         // - Loop through and print each item
-        for (int i = 0; i < products.length;i++) {
+        for (int i = 0; i < products.length; i++) {
             System.out.println(products[i]);
         }
 
         // - Call equals() to compare two products with the same ID and name
+        Product product2 = new Product("Spoon", 1.09);
+        System.out.println("Comparison: " + product.equals(product2));
         System.out.println("Comparison: " + product.equals(product));
-        System.out.println("Comparison: " + product.equals(gamingThing));
-        System.out.println("Comparison: " + product.equals(foods));
-        System.out.println("Comparison: " + product.equals(plastic));
-    
+        
+        //This is for coupoun class yuh (Q3 at bottom) to apply it
+        //Making gamingThing on a discount here cause nintendo wont make that happen in real life :(
+        //"At least I can cut the price in half in vs code" - What I said while typing this
         double discountVar = Coupon.discountRate;
-        gamingThing.price -= gamingThing.price*discountVar
+        gamingThing.price -= gamingThing.price * discountVar;
         System.out.println(gamingThing);
+
+        gamingThing.warrantyInfo();
     }
 }
 
@@ -185,16 +176,21 @@ class SuperStoreTest {
 // You are restricted, and thus unable to extend that class
 
 // 2. Make a class Coupon with a final discountRate and apply it to a Product
-
 class Coupon {
     final static double discountRate = .45;
+
+    public static void applyDiscount(Product product) {
+        product.price -= product.price * discountRate;
+    }
 }
 
 // 3. Add a method to Electronics called warrantyInfo() and mark it final
-//ok done
-
+//ok done in electronics class on line uhhh 83!
 
 // 4. Use access modifiers appropriately and explain your choices in comments
 //ok, I mainly used public cause it was the least restrictive,
 // and in them I use a jumble of private and protected cause I dont want other classes outside the ones I made access my variables
 //UNLESS i soecifically say in the initalization to access them
+//For example, productId needed to be final cause it doesnt change
+//But uhhhh something like price its protected, not changed by outside forces but by company (or when tariffs hit and the switch 2 is 450 dollars why)
+//i think i answered this well huzzah!
